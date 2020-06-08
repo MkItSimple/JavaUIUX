2e2c79040b85d8ea492b8fd3c91fe1811c72cc93
MkItSimple <choanania1@gmail.com>
2020-06-08 22:45:08 +0800

    CustomToolbar-Sample2

--- app/src/main/java/com/example/javauiux/MainActivity.java
+++ app/src/main/java/com/example/javauiux/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.javauiux;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.p1){
+            Toast.makeText(getApplicationContext(), "You clicked Page 1", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
